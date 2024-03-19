47c4c62145b809a9d463fac37766610e63ffcc88
MuhammadAbdullah191 <abdullah.badar@devsinc.com>
2024-03-19 15:16:05 -0700

    Updated to create two deliveries

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get("/", function (req, res) {
 app.post("/move-order-to-shipday", function (req, res) {
   console.log("moving order");
   console.log(req.body);
-  console.log(req.body.orderDetails[0].product)
+  console.log(req.body.orderDetails)
   let payload = req.body;
 
   let deliveryTime = payload.job_delivery_datetime.split(' ');
